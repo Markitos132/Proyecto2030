788e4bb0c84b0b2dd77a44ab1aadc709b6eebca6
Markitos <micota203@gmail.com>
2026-09-25 20:27:38 -0300

    no se que paso

--- app/Http/Controllers/IngestaController.php
+++ app/Http/Controllers/IngestaController.php
@@ -170,12 +170,12 @@ class IngestaController extends Controller
             'temperatura' => $datos['temperatura'],
         ]);
 
-        if ($sesion->$estado === Sesion::ESTADO_FINALIZAD && (!$sesion->$fecha_fin || $momento > $ $sesion->$fecha_fin)) {
+        if ($sesion->estado === Sesion::ESTADO_FINALIZAD && (!$sesion->$fecha_fin || $momento > $ $sesion->$fecha_fin)) {
             $sesion->update([
                 'fecha_fin' => $momento,
-                'duracion_sesion' => $sesion->$fecha_inicio
-                                    ? (int) round($sesion->$fecha_inicio->diffInMinutes($momento))
-                                    : $sesion->$duracion_sesion,
+                'duracion_sesion' => $sesion->fecha_inicio
+                                    ? (int) round($sesion->fecha_inicio->diffInMinutes($momento))
+                                    : $sesion->duracion_sesion,
             ]);
         }
 
